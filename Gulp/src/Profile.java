ce0027363bf45c7c8bc78c343003b727adb66da1
Brandon Sanders <btsanders04@gmail.com>
2015-08-25 15:48:40 -0400

    Profile

--- Gulp/src/Profile.java
+++ Gulp/src/Profile.java
@@ -1,11 +1,14 @@
 
 
 import java.io.IOException;
+import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Profile
@@ -13,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Profile")
 public class Profile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+       private String profiles = "";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,7 +29,50 @@ public class Profile extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession(true);
+		profiles="";
+		User user = (User) session.getAttribute("User");
+		Integer id = user.getUser_id();
+	
+	try {
+			DBQuery.openConnection();
+			String sql2 = "select * from userprofile where user_id="+id;
+			System.out.println(sql2);
+			ResultSet result2 = DBQuery.getFromDB(sql2);
+			result2.next();
+			profiles+="<div class=\"container\"><div class=\"item  col-xs-4 col-lg-4\">  <div class=\"panel panel-primary\"><div class=\"panel-heading\">"+result2.getString("USER_ID") 
+					+"</div><div class=\"panel-body\"> <p class=\"group inner list-group-item-text\">"+result2.getString("USER_NAME") 
+						+"</p> <p class=\"group inner list-group-item-text\"> "+result2.getString("USER_EMAIL") 
+						+"</p> <p class=\"group inner list-group-item-text\"> "+result2.getString("USER_ZIPCODE") 
+					+"</p></div></div></div></div><div class=\"container\">";
+			String sql = "select * from review where USER_ID="+id;
+			
+			ResultSet result = DBQuery.getFromDB(sql);
+			while(result.next())
+			{
+				String sql3 = "select restaurant_name from restaurants where RESTAURANT_ID="+result.getString("restaurant_id");
+				ResultSet result3 = DBQuery.getFromDB(sql3);
+				result3.next();
+				profiles+=" <div class=\"panel panel-primary\"><div class=\"panel-heading\">"+result3.getString("restaurant_name")+"</div>"+
+			     " <div class=\"panel-body\"><p > Date: "+result.getDate("REVIEW_DATE")
+						+"</p><p>  Rating: "+result.getString("RATING")+"</p><p>"
+			     +result.getString("REVIEW_DES")+"</p></div></div>";
+			
+				}
+			profiles+="</div>";
+
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		request.setAttribute("profiles", profiles);
 		getServletContext().getRequestDispatcher("/Profile.jsp").forward(request,
 				response);
 	}
@@ -35,7 +81,6 @@ public class Profile extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request,response);
 	}
-
 }
