17be50fb5c86d180047c0e71565d590c00421e3d
Brandon Sanders <btsanders04@gmail.com>
2015-08-25 09:12:19 -0400

    added User Class

---
+++ Gulp/src/User.java
@@ -0,0 +1,18 @@
+
+public class User {
+
+	
+	private String name,email,zipcode,user_id;
+	
+	public User(String id){
+		user_id=id;
+	}
+	
+	
+	public void setDetails(String n, String e, String zip){
+		n=name;
+		email=e;
+		zipcode=zip;
+	}
+	
+}
